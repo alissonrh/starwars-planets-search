473d79657a8038f5e0b34a0f6866e8148c08563b
alissonRH <alisson.honorato@live.com>
2022-07-08 15:46:54 -0300

    requsição api e delete chave

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Provider from './context/myProvider';
+import Table from './components/Table';
 
 function App() {
   return (
-    <span>Hello !</span>
+    <Provider>
+      <Table />
+    </Provider>
   );
 }
 
---
+++ src/context/myProvider.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import MyContext from './myContext';
+
+/* const INITIAL_STATE = { nome: 'Xablau', idade: 100 }; */
+
+function Provider({ children }) {
+  const [state, setState] = useState();
+
+  const deleteKey = (data) => {
+    data.forEach((element) => {
+      delete element.residents;
+    });
+    return data;
+  };
+
+  useEffect(() => {
+    const getPlanets = () => {
+      fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.results);
+          const returnDele = deleteKey(data.results);
+          console.log(returnDele);
+          setState(returnDele);
+        })
+        .catch((error) => {
+          setState({
+            error,
+          });
+        });
+    };
+    getPlanets();
+  }, []);
+
+  return (
+    <MyContext.Provider value={ state }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
