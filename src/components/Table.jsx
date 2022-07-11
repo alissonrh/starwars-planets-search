e43fafc02b18423b26b1ab72ddd95b475eaa027c
alissonRH <alisson.honorato@live.com>
2022-07-11 17:26:40 -0300

    1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import context from '../context/myContext';
 
 function Table() {
+  const date = useContext(context);
+  console.log(date);
   return (
-    <p>tesre</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      {date !== undefined && (
+        <tbody>
+          {date.map((e) => (
+            <tr key={ e.name }>
+              <td>{ e.name }</td>
+              <td>{e.rotation_period}</td>
+              <td>{e.orbital_period}</td>
+              <td>{e.diameter}</td>
+              <td>{e.climate}</td>
+              <td>{e.gravity}</td>
+              <td>{e.terrain}</td>
+              <td>{e.surface_water}</td>
+              <td>{e.population}</td>
+              <td>
+                {e.films.map((e2, index) => (
+                  <p key={ index }>{e2}</p>
+                ))}
+              </td>
+              <td>{e.created}</td>
+              <td>{e.edited}</td>
+              <td>{e.url}</td>
+            </tr>
+          ))}
+        </tbody>)}
+      {/*   <tbody>
+        {state.map((e) => (
+          <tr key={ e.name }>
+            <td>{e.rotation_period}</td>
+            <td>{e.orbital_period}</td>
+            <td>{e.diameter}</td>
+            <td>{e.clumate}</td>
+            <td>{e.gravity}</td>
+            <td>{e.terrain}</td>
+            <td>{e.surface_water}</td>
+            <td>{e.population}</td>
+            <td>
+              {e.films.map((e2, index) => (
+                <p key={ index }>{e2}</p>
+              ))}
+            </td>
+            <td>{e.created}</td>
+            <td>{e.edited}</td>
+            <td>{e.url}</td>
+          </tr>
+        ))}
+      </tbody> */}
+    </table>
   );
 }
 
